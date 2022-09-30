964c85980302b21cf096fb6f953bb4cc6e7385aa
t-star <raiki.togawa@paiza.co.jp>
2022-10-01 02:18:04 +0900

    arrange: get-cert-info-and-get-url

--- js/getPeerCertificateInfoTest2.js
+++
@@ -1,9 +0,0 @@
-async function getPeerCertificateInfo() {
-    sourceCode = "openssl s_client -connect www.dendai.ac.jp:443 -showcerts < /dev/null 2>&1 | grep '[si]:'";
-    lang = "bash";
-    input = "";
-    apiKey = "guest";
-    console.log(await executeCode(sourceCode, lang, input, apiKey));    
-}
-
-getPeerCertificateInfo();
