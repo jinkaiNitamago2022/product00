58aac20a0291fc806bb807cc2c698aee0a763f1b
t-star <raiki.togawa@paiza.co.jp>
2022-10-02 02:08:44 +0900

    modify: when CN, O undefined

--- js/pkg/content.js
+++ js/pkg/content.js
@@ -1,5 +1,5 @@
 import * as certInfo from './certificate-Info.js';
-import * as storage from './storage.js';
+// import * as storage from './storage.js';
 import * as url from './url.js';
 
 const storageCache = {};
@@ -14,17 +14,16 @@ export async function updateTab(tab) {
     )
     chrome.storage.session.set(
         {
-            'commonName': commonName,
-            'organization': organization
+            'commonName': typeof commonName !== 'undefined' ? commonName : 'Unknown',
+            'organization': typeof organization !== 'undefined' ? organization : 'Unknown'
             // 後に脅威情報リンクも追記する
         }
     );
-    storage.assignStorageCache(storageCache);
 
     console.log('commonName: ' + commonName);
     console.log('Organization: ' + organization);
 
     // ↓ のような感じで、storageCache に保存した情報を取得する
-    // storage.assignStorageCache(storageCache);
+    // await storage.assignStorageCache(storageCache);
     // console.log(storageCache);
 }
