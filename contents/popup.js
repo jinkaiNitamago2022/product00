e443a5368c0e167b106024d31d23d6e8098ce12d
t-star <raiki.togawa@paiza.co.jp>
2022-10-02 02:02:25 +0900

    update: use /js/pkg/storage.js

--- contents/popup.js
+++ contents/popup.js
@@ -1,27 +1,9 @@
+import * as storage from '../js/pkg/storage.js';
+
 const storageCache = {};
 
-chrome.action.onClicked.addListener(async(tab) => {
-    await getAllStorageSyncData().then((items) => {
-        Object.assign(storageCache, items);
-        console.log(storageCache);
-    });
-});
-
 document.addEventListener('DOMContentLoaded', async(event) => {
-    await getAllStorageSyncData().then((items) => {
-        Object.assign(storageCache, items);
-    });
+    await storage.assignStorageCache(storageCache);
 
     document.getElementById('ssl-organization').innerHTML = storageCache.organization;
 })
-
-function getAllStorageSyncData() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get(null, (items) => {
-            if (chrome.runtime.lastError) {
-                return reject(chrome.runtime.lastError);
-            }
-            resolve(items);
-        });
-    });
-}
