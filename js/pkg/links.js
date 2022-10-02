b2c82874ffdffd8460bb4b928de97ce93bc6edaf
sumeshi <sum3sh1@protonmail.com>
2022-10-02 21:38:41 +0900

    feat: add vt-url link generate func

--- js/pkg/links.js
+++ js/pkg/links.js
@@ -1,19 +1,24 @@
 import * as urlMod from './url.js';
+import * as hashMod from './hash.js';
 
 const VIRUSTOTAL_BASE = 'https://www.virustotal.com/gui'
 
-export function generateVirusTotalDomainLink(domain='') {
+export async function generateVirusTotalDomainLink(domain='') {
     return `${VIRUSTOTAL_BASE}/domain/${domain}`;
 }
 
-export function generateAllLinks(tab) {
+const generateVirusTotalUrlLink = async (url) => {
+    const hash = await hashMod.calcSha256HashValue(url)
+    return `${VIRUSTOTAL_BASE}/url/${hash}`;
+}
+
+export async function generateAllLinks(tab) {
     var url = urlMod.getCurrentTabUrl(tab);
     var [_, domain, _] = urlMod.breakUpUrl(url);
-    console.log(generateVirusTotalDomainLink(domain));
 
     return [
-        // generateVirusTotalUrlLink(url),  // 関数名は好きに変えてください
-        generateVirusTotalDomainLink(domain),
+        await generateVirusTotalUrlLink(url),
+        await generateVirusTotalDomainLink(domain),
         // generateDigicertLink(domain)  // 関数名は好きに変えてください
     ];
 }
