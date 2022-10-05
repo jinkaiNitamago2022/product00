0bd3f480b57d6de3a34170b6f9eb4ad1a1095479
t-star <raiki.togawa@paiza.co.jp>
2022-10-05 19:07:16 +0900

    update: popup

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,28 @@
 import * as storage from './pkg/storage.js';
 
-const storageCache = {};
+const COLOR_THEME = {
+    'danger': 'red',
+    'safety': 'rgb(25, 135, 84)'
+};
 
 document.addEventListener('DOMContentLoaded', async(event) => {
+    let storageCache = {};
     await storage.assignStorageCache(storageCache);
 
+    var theme = '';
     document.getElementById('ssl-organization').innerHTML = storageCache.organization;
+    if (storageCache.organization === 'Unknown') {
+        theme = 'danger';
+    } else {
+        theme = 'safety';
+    }
+    document.getElementById('ssl-organization').style.color = COLOR_THEME[theme];
+    document.getElementsByTagName('body')[0].style.backgroundColor = COLOR_THEME[theme];
+    Array.prototype.forEach.call(document.getElementsByTagName('hr'), (ele) => {
+        ele.style.backgroundColor = COLOR_THEME[theme];
+    });
+
+    document.getElementById('virus-total-url-link').setAttribute('href', storageCache.virusTotalUrlLink);
+    document.getElementById('virus-total-domain-link').setAttribute('href', storageCache.virusTotalDomainLink);
+    document.getElementById('digicert-link').setAttribute('href', storageCache.digicertLink);
 })
